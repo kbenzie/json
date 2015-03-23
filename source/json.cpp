11467e9844ffd324f618fc1279e1e684f657b98d
Kenneth Benzie (Benie) <k.benzie@codeplay.com>
2015-03-23 23:38:16 +0000

    Add API function stubs & test stub

---
+++ source/json.cpp
@@ -0,0 +1,9 @@
+#include <json/json.hpp>
+
+std::string write(const json::value &value, const char *indent = "  ") {
+  return std::string();
+}
+
+json::value read(const std::string &string) {
+  return json::value();
+}
---
+++ test/main.cpp
@@ -0,0 +1,3 @@
+#include <json/json.hpp>
+
+int main(int argc, char **argv) { return 0; }
